bf29eb79670e28c9c9b6bd63670c929c5a4667ca
Eugénie <eugenie.b24@hotmail.fr>
2024-08-23 21:03:02 +0100

    Update context to be in BlogPage.js and drill the prop in lower components

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Comment from "./Comment";
 
-export default function Post() {
+export default function Post({ username, isAdmin }) {
   return (
     <div>
+      {isAdmin && <button>Delete</button>}
+
       <h2>Example Post Title</h2>
       <p>This is a post content.</p>
 
-      <Comment></Comment>
+      <Comment username={username} isAdmin={isAdmin}></Comment>
     </div>
   );
 }
